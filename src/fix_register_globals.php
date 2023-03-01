70b70f325dc53d7ef8e500261bdf74277d2d978f
wiboonsak_jai <wiboonsak.uinfo@gmail.com>
2023-03-01 21:17:25 +0700

    feat: add ereg* and split*

--- src/fix_register_globals.php
+++ src/fix_register_globals.php
@@ -76,3 +76,10 @@ if (!function_exists('session_register')) {
     function session_unregister($var_name)
     {   unset($_SESSION[$var_name]); }
 }
+
+if(!function_exists('ereg'))            { function ereg($pattern, $subject, &$matches = []) { return preg_match('/'.$pattern.'/', $subject, $matches); } }
+if(!function_exists('eregi'))           { function eregi($pattern, $subject, &$matches = []) { return preg_match('/'.$pattern.'/i', $subject, $matches); } }
+if(!function_exists('ereg_replace'))    { function ereg_replace($pattern, $replacement, $string) { return preg_replace('/'.$pattern.'/', $replacement, $string); } }
+if(!function_exists('eregi_replace'))   { function eregi_replace($pattern, $replacement, $string) { return preg_replace('/'.$pattern.'/i', $replacement, $string); } }
+if(!function_exists('split'))           { function split($pattern, $subject, $limit = -1) { return preg_split('/'.$pattern.'/', $subject, $limit); } }
+if(!function_exists('spliti'))          { function spliti($pattern, $subject, $limit = -1) { return preg_split('/'.$pattern.'/i', $subject, $limit); } }
